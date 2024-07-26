55121ee84411c96971215b6ff09acea820214349
Dillon Min <dillon.minfei@gmail.com>
2024-07-26 18:59:13 +0800

    update usr-wifi232-t
    MOST IMPORTANT: Timer::after_millis(1500).await;
    
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- examples/stm32f1/src/bin/blinky.rs
+++ examples/stm32f1/src/bin/blinky.rs
@@ -7,6 +7,7 @@ use embassy_stm32::usart::{Config, Uart};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_stm32::gpio::{Level, Output, Speed, Pin, AnyPin};
 use embassy_time::Timer;
+use embassy_stm32::time::Hertz;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
@@ -28,7 +29,26 @@ async fn blinky(pin: AnyPin) {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            // Oscillator for bluepill, Bypass for nucleos.
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll = Some(Pll {
+            src: PllSource::HSE,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL9,
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+    }
+    let p = embassy_stm32::init(config);
+    //let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
     spawner.spawn(blinky(p.PB0.degrade())).unwrap();
@@ -38,27 +58,28 @@ async fn main(spawner: Spawner) {
     let mut rst = Output::new(p.PA0, Level::High, Speed::Low);
     Timer::after_millis(200).await;
     rst.set_low();
-    Timer::after_millis(100).await;
+    Timer::after_millis(300).await;
     rst.set_high();
-    Timer::after_millis(100).await;
+    Timer::after_millis(1500).await;
 
-    let mut s  = [0u8; 16];
+    let mut s  = [0u8; 1000];
+    let mut t  = [0u8; 20];
     let mut a  = [0u8; 1];
-    unwrap!(usart.write(b"+++").await);
+    unwrap!(usart.write("+++".as_bytes()).await);
+    //Timer::after_millis(100).await;
+    info!("0\n");
     unwrap!(usart.read(&mut a).await);
-    info!("1 {}\n", a);
-    unwrap!(usart.write(b"a").await);
-    //Timer::after_millis(10).await;
-    unwrap!(usart.write(b"at+e\r").await);
+    let str_resp = core::str::from_utf8(&a).unwrap();
+    info!("1 {}\n", str_resp);
+    unwrap!(usart.write("a".as_bytes()).await);
+    Timer::after_millis(10).await;
+    unwrap!(usart.write(b"at+h\r").await);
     unwrap!(usart.read(&mut s).await);
     let str_resp = core::str::from_utf8(&s).unwrap();
     info!("2 {}", str_resp);
-    unwrap!(usart.read(&mut s).await);
-    let str_resp = core::str::from_utf8(&s).unwrap();
-    info!("{}", str_resp);
-    unwrap!(usart.read(&mut s).await);
-    let str_resp = core::str::from_utf8(&s).unwrap();
-    info!("{}", str_resp);
+    unwrap!(usart.read(&mut t).await);
+    let str_resp = core::str::from_utf8(&t).unwrap();
+    info!("3 {}", str_resp);
     loop {
      //   led.set_high();
         Timer::after_millis(1000).await;
