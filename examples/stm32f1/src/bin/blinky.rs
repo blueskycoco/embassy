f3e83fdd7d12621c529d63c688ae918abc737af0
Dillon Min <dillon.minfei@gmail.com>
2024-09-20 14:49:23 +0800

    format code for debug
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- examples/stm32f1/src/bin/blinky.rs
+++ examples/stm32f1/src/bin/blinky.rs
@@ -14,6 +14,10 @@ bind_interrupts!(struct Irqs {
     USART2 => usart::InterruptHandler<peripherals::USART2>;
 });
 
+fn clear(ary: &mut [u8]) {
+    ary.iter_mut().for_each(|m| *m = 0)
+}
+
 #[embassy_executor::task]
 async fn blinky(pin: AnyPin) {
     let mut led = Output::new(pin, Level::High, Speed::Low);
@@ -27,12 +31,9 @@ async fn blinky(pin: AnyPin) {
     }
 }
 
-fn clear(ary: &mut [u8]) {
-    ary.iter_mut().for_each(|m| *m = 0)
-}
-
-async fn usr_cmd(usart: &mut Uart<'_, embassy_stm32::mode::Async>, cmd: &str, s: &mut [u8]) {
-    //let mut s = [0u8; 128];
+async fn usr_cmd(usart: &mut Uart<'_, embassy_stm32::mode::Async>, cmd: &str,
+                 s: &mut [u8]) {
+    clear(s);
     unwrap!(usart.write(cmd.as_bytes()).await);
     loop {
         unwrap!(usart.read_until_idle(s).await);
@@ -44,7 +45,7 @@ async fn usr_cmd(usart: &mut Uart<'_, embassy_stm32::mode::Async>, cmd: &str, s:
         clear(s);
     }
 }
-
+/*
 async fn usr_init(usart: &mut Uart<'_, embassy_stm32::mode::Async>) -> bool {
     let mut s = [0u8; 128];
     unwrap!(usart.write("+++".as_bytes()).await);
@@ -64,7 +65,8 @@ async fn usr_init(usart: &mut Uart<'_, embassy_stm32::mode::Async>) -> bool {
             info!("{}", str_resp);
             if str_resp.contains("Success") {
                 usr_cmd(usart, "at+wann\r", &mut s).await;
-                usr_cmd(usart, "at+netp=tcp,server,1234,172.20.10.8\r", &mut s).await;
+                usr_cmd(usart, "at+netp=tcp,server,1234,172.20.10.8\r", &mut s)
+                        .await;
                 usr_cmd(usart, "at+netp\r", &mut s).await;
                 //usr_cmd(usart, "at+tcpdis=on\r").await;
                 usr_cmd(usart, "at+tcpdis\r", &mut s).await;
@@ -78,7 +80,7 @@ async fn usr_init(usart: &mut Uart<'_, embassy_stm32::mode::Async>) -> bool {
         Timer::after_millis(1000).await;
     }
 }
-
+*/
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = embassy_stm32::Config::default();
@@ -103,37 +105,38 @@ async fn main(spawner: Spawner) {
     //let p = embassy_stm32::init(Default::default());
 
     spawner.spawn(blinky(p.PB0.degrade())).unwrap();
-    let mut s = [0u8; 128];
     let config = Config::default();
     let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
     let mut rst = Output::new(p.PA0, Level::High, Speed::Low);
+
+    // reset usr_wifi232_t
     Timer::after_millis(200).await;
     rst.set_low();
     Timer::after_millis(300).await;
     rst.set_high();
     Timer::after_millis(1500).await;
+
+    // enter at command mode
     let mut s = [0u8; 128];
     unwrap!(usart.write("+++".as_bytes()).await);
     unwrap!(usart.read_until_idle(&mut s).await);
     unwrap!(usart.write("a".as_bytes()).await);
     unwrap!(usart.read_until_idle(&mut s).await);
-
+    // waiting finish
     Timer::after_millis(300).await;
+
     usr_cmd(&mut usart, "at+wmode=apsta\r", &mut s).await;
-    usr_cmd(&mut usart, "at+netp=TCP,Server,1234,172.20.10.2\r", &mut s).await;
+    //usr_cmd(&mut usart, "at+netp=TCP,Server,1234,172.20.10.2\r", &mut s).await;
     usr_cmd(&mut usart, "at+tcpdis=on\r", &mut s).await;
+
     loop {
         let mut ss = [0u8; 128];
         usr_cmd(&mut usart, "at+wann\r", &mut s).await;
-        clear(&mut s);
         usr_cmd(&mut usart, "at+netp\r", &mut s).await;
-        clear(&mut s);
         usr_cmd(&mut usart, "at+tcplk\r", &mut s).await;
         let tcplk = core::str::from_utf8(&s).unwrap();
-        info!("{}", tcplk);
-        usr_cmd(&mut usart, "at+ping=172.20.10.6\r", &mut ss).await;
+        usr_cmd(&mut usart, "at+ping=172.20.10.13\r", &mut ss).await;
         let ping = core::str::from_utf8(&ss).unwrap();
-        info!("{}", ping);
         if ping.contains("Success") && tcplk.contains("on") {
             info!("network stable!");
             usr_cmd(&mut usart, "at+entm\r", &mut s).await;
@@ -141,17 +144,4 @@ async fn main(spawner: Spawner) {
         }
         Timer::after_millis(2000).await;
     }
-/*
-    usr_init(&mut usart).await;
-
-    info!("going to do network");
-    loop {
-        usr_cmd(&mut usart, "at+tcplk\r").await;
-/*        usr_cmd(&mut usart, "at+tcpto\r").await;
-        usr_cmd(&mut usart, "at+tcpdis\r").await;
-        usr_cmd(&mut usart, "at+maxsk\r").await;
-        usr_cmd(&mut usart, "at+netp\r").await;
-        usr_cmd(&mut usart, "at+wslq\r").await;*/
-        Timer::after_millis(1000).await;
-    }*/
 }
