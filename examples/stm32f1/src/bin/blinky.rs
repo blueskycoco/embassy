0639d9e14e0d4b8803b1781975960298a0c7fe27
Dillon Min <dillon.minfei@gmail.com>
2024-09-24 10:33:42 +0800

    user BufferedUart
    └─ blinky::usr_cmd::{async_fn#0} @ src/bin/blinky.rs:58
    88.432250 INFO  AT+WANN
    +ok=DHCP,0.0.0.0,0.0.0.0,0.0.0.0
    
    └─ blinky::usr_cmd::{async_fn#0} @ src/bin/blinky.rs:58
    88.447235 INFO  AT+NETP
    +ok=TCP,Server,1234,172.20.10.2
    
    └─ blinky::usr_cmd::{async_fn#0} @ src/bin/blinky.rs:58
    88.460327 INFO  AT+TCPLK
    +ok=off
    
    └─ blinky::usr_cmd::{async_fn#0} @ src/bin/blinky.rs:58
    89.259277 INFO  AT+PING=172.20.10.13
    +ok=Timeout
    
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- examples/stm32f1/src/bin/blinky.rs
+++ examples/stm32f1/src/bin/blinky.rs
@@ -3,15 +3,18 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::usart::{Config, Uart};
+use embassy_stm32::usart::{Config, BufferedUart, BufferedUartRx, BufferedUartTx};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_stm32::gpio::{Level, Output, Speed, Pin, AnyPin};
 use embassy_time::Timer;
 use embassy_stm32::time::Hertz;
+use embedded_io_async::BufRead;
+use embedded_io_async::{Read, Write};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    USART2 => usart::InterruptHandler<peripherals::USART2>;
+    USART2 => usart::BufferedInterruptHandler<peripherals::USART2>;
 });
 
 fn clear(ary: &mut [u8]) {
@@ -31,18 +34,26 @@ async fn blinky(pin: AnyPin) {
     }
 }
 
-async fn usr_cmd(usart: &mut Uart<'_, embassy_stm32::mode::Async>, cmd: &str,
-                 s: &mut [u8]) {
+async fn usr_cmd(rx: &mut BufferedUartRx<'_>,
+                    tx: &mut BufferedUartTx<'_>,
+                    cmd: &str,
+                    s: &mut [u8]) {
     clear(s);
-    unwrap!(usart.write(cmd.as_bytes()).await);
+    unwrap!(tx.write_all(cmd.as_bytes()).await);
+    let mut cnt = 0;
     loop {
-        unwrap!(usart.read_until_idle(s).await);
-        let str_resp = core::str::from_utf8(s).unwrap();
-        info!("{}", str_resp);
-        if str_resp.contains("+ok") || str_resp.contains("+ERR") {
+        let n = rx.read(&mut s[cnt..]).await;
+        match n {
+            Ok(bytes) => cnt = cnt + bytes,
+            Err(e) => info!("read error {}", e),
+        }
+
+        if s.get(cnt - 4) == Some(&b'\r') && s.get(cnt - 3) == Some(&b'\n') &&
+           s.get(cnt - 2) == Some(&b'\r') && s.get(cnt - 1) == Some(&b'\n') {
+            let str_resp = core::str::from_utf8(s).unwrap();
+            info!("{}", str_resp);
             break;
         }
-        clear(s);
     }
 }
 /*
@@ -106,7 +117,15 @@ async fn main(spawner: Spawner) {
 
     spawner.spawn(blinky(p.PB0.degrade())).unwrap();
     let config = Config::default();
-    let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7, p.DMA1_CH6, config).unwrap();
+    static TX_BUF: StaticCell<[u8; 128]> = StaticCell::new();
+    let tx_buf = &mut TX_BUF.init([0; 128])[..];
+    static RX_BUF: StaticCell<[u8; 128]> = StaticCell::new();
+    let rx_buf = &mut RX_BUF.init([0; 128])[..];
+    let usart = BufferedUart::new(p.USART2, Irqs, p.PA3, p.PA2, tx_buf,
+                                      rx_buf, config).unwrap();
+    let (mut usr_tx, mut usr_rx) = usart.split();
+    //let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, p.DMA1_CH7,
+    //p.DMA1_CH6, config).unwrap();
     let mut rst = Output::new(p.PA0, Level::High, Speed::Low);
 
     // reset usr_wifi232_t
@@ -118,30 +137,34 @@ async fn main(spawner: Spawner) {
 
     // enter at command mode
     let mut s = [0u8; 128];
-    unwrap!(usart.write("+++".as_bytes()).await);
-    unwrap!(usart.read_until_idle(&mut s).await);
-    unwrap!(usart.write("a".as_bytes()).await);
-    unwrap!(usart.read_until_idle(&mut s).await);
+    unwrap!(usr_tx.write_all("+++".as_bytes()).await);
+    unwrap!(usr_rx.read(&mut s).await);
+    unwrap!(usr_tx.write_all("a".as_bytes()).await);
+    unwrap!(usr_rx.read(&mut s).await);
     // waiting finish
-    Timer::after_millis(300).await;
-
-    usr_cmd(&mut usart, "at+wmode=apsta\r", &mut s).await;
+    Timer::after_millis(500).await;
+    usr_cmd(&mut usr_rx, &mut usr_tx, "at+wmode=sta\r", &mut s).await;
     //usr_cmd(&mut usart, "at+netp=TCP,Server,1234,172.20.10.2\r", &mut s).await;
-    usr_cmd(&mut usart, "at+tcpdis=on\r", &mut s).await;
+    usr_cmd(&mut usr_rx, &mut usr_tx, "at+tcpdis=on\r", &mut s).await;
 
     loop {
         let mut ss = [0u8; 128];
-        usr_cmd(&mut usart, "at+wann\r", &mut s).await;
-        usr_cmd(&mut usart, "at+netp\r", &mut s).await;
-        usr_cmd(&mut usart, "at+tcplk\r", &mut s).await;
+        usr_cmd(&mut usr_rx, &mut usr_tx, "at+wann\r", &mut s).await;
+        usr_cmd(&mut usr_rx, &mut usr_tx, "at+netp\r", &mut s).await;
+        usr_cmd(&mut usr_rx, &mut usr_tx, "at+tcplk\r", &mut s).await;
         let tcplk = core::str::from_utf8(&s).unwrap();
-        usr_cmd(&mut usart, "at+ping=172.20.10.13\r", &mut ss).await;
+        usr_cmd(&mut usr_rx, &mut usr_tx, "at+ping=172.20.10.13\r", &mut ss).await;
         let ping = core::str::from_utf8(&ss).unwrap();
         if ping.contains("Success") && tcplk.contains("on") {
             info!("network stable!");
-            usr_cmd(&mut usart, "at+entm\r", &mut s).await;
+            usr_cmd(&mut usr_rx, &mut usr_tx, "at+entm\r", &mut s).await;
             break;
         }
         Timer::after_millis(2000).await;
+    }
+    let mut pic  = [0u8; 7840];
+    loop {
+        unwrap!(usr_tx.write_all("send ok".as_bytes()).await);
+        unwrap!(usr_rx.read_exact(&mut pic).await);
     }
 }
